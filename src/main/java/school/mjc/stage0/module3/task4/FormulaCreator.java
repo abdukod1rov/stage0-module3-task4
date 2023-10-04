2d1b977c7995859b972b949c2b78ae829d6b3699
samandar7 <abdukodirovsamandar8@gmail.com>
2023-10-04 23:16:56 +0500

    Formula might be wrong

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 2.5;
+        double b = 1.8;
+
+        double formula = (9 * Math.pow(a, 2) - 5 + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] chars = {'c', 'h', 'a', 'r'};
+
+        for (char ch: chars){
+            int intValue = (int) ch;
+            System.out.println("Character: " + ch + ", Integer representation: " + intValue);
+
+        }
     }
 }
